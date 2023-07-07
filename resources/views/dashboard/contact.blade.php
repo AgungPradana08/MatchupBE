cb022e41b88a714fec713be82eb15f62a90a9539
Your Name <you@example.com>
2023-07-07 22:52:01 +0700

    [Riko] Responsive Sparring

--- resources/views/dashboard/contact.blade.php
+++ resources/views/dashboard/contact.blade.php
@@ -2,43 +2,55 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Match Up</title>
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/contact.css">
 </head>
 <body>
-    <section class="navbar" >
-        <div class="navbar-left">
-            <img style="margin-left: 7%;" class="logo" src="css/img/logo.png" alt="">
-            <!-- <div style="margin-left: 7%;" class="logo">
-
-            </div> -->
-            <a class="home" href="/" >Home</a>
-            <a class="tentang" href="/about" style="color: #FE6B00;;" >tentang</a>
-            <a class="kontak" href="#" >Kontak</a>
+    <nav class="navbar navbar-expand-lg p-0 position-fixed bg-white " style="width: 100vw; z-index: 100;">
+        <div class="container bg-ms-primary ">
+          <a class="navbar-brand" href="#"><img src="/css/img/logo.png" style="height: 6vh;" alt=""></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse"  id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item mx-0 mx-lg-2">
+                <a class="nav-link text-start text-lg-center active" aria-current="page" href="/"><span>Home</span></a>
+              </li>
+              <li class="nav-item mx-0 mx-lg-2">
+                <a class="nav-link text-start text-lg-center " aria-current="page" href="/about"><span>Tentang</span></a>
+              </li>
+              <li class="nav-item mx-0 mx-lg-2">
+                <a class="nav-link text-start text-lg-center " aria-current="page" href="/contact">Kontak</a>
+              </li>
+              
+            </ul>
+            <div>
+                <a class="btn d-block d-lg-inline-block pt-1 pb-1 mb-2 mb-lg-0 text-start text-lg-center signup" href="/register" type="submit">Sign Up</a>
+                <a class="btn d-block d-lg-inline-block pt-1 pb-1 login text-start text-lg-center" type="submit" href="/login" >Log In</a>
+            </div>
+          </div>
         </div>
-        <div class="navbar-right">
-            <a class="sign-up" href="signup.html">Sign Up</a>
-            <a class="log-in" style="margin-right: 20%; margin-left: 5%; background-color: #FE6B00;; color: white; " href="/login/">Log In</a>
+    </nav>
+    <div class="container ">
+        <div class="row contact">
+            <div class="col-6  d-none d-lg-flex justify-content-center align-items-center">
+                <div class="img" ></div>
+            </div>
+            <div class="col-12 col-lg-6 contact-content">
+                <h1>CONTACT</h1>
+                <div class="wrapper ">
+                    <input style="grid-area: input1;" placeholder="Masukkan Email..." type="text" value="">
+                    <input style="grid-area: input2;" placeholder="Masukkan Nama Pengguna..." type="text" value="">
+                    <textarea style="grid-area: input3;" placeholder="Masukkan Saran atau Kritik..." name="" id="" cols="30" rows="10"></textarea>
+                    <button style="grid-area: button; resize: none;" >Kirim Pesan</button>
+                </div>
+            </div>
         </div>
-    </section>
-    <section class="contact">
-        <div class="contact-1-left">
-            <img src="css/img/contact-image.png" alt="">
-            <!-- <div class="image1">
-            
-            </div> -->
-        </div>
-        <div class="contact-1-right">
-            <p style="font-size: 60px;" >KONTAK</p>
-            <form action="">
-                <input class="input-form" type="text" name="name" placeholder="Nama Penulis...">
-                <input class="input-form" type="text" name="email" placeholder="Alamat Email...">
-            </form>
-            <textarea style="margin-bottom: 20px;" name="saran" cols="30" rows="10" placeholder="Saran atau Kritik..."></textarea>
-            <button style="height: 40px; font-size: 20px; text-transform: uppercase; font-family: opensans-bold; background-color: #FE6B00; border: transparent; color: white; " >Kirim Pesan</button>
-        </div>
-    </section>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
 </body>
 </html>
