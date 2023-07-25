fd7e58bcb9fa7b9ae6db9964312f89b93fad3ac2
Agung_Pradana_08 <izunna158@gmail.com>
2023-07-25 07:01:49 +0700

    [Agung] Adding Favicon

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -6,6 +6,7 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/home.css">
+    <link rel="shortcut icon" type="image/x-icon" href="/css/img/favicon.ico">
 </head>
 <body>
   <nav class="navbar navbar-expand-lg p-0 position-fixed bg-white " style="width: 100vw; z-index: 100;">
