0d6732664055abe21aec1be615532beab04b0bfe
Agung_Pradana_08 <izunna158@gmail.com>
2023-07-31 10:19:58 +0700

    [Agung] Change Header Aksebilitas to Tingkatan

--- resources/views/user/usersparring/tambahsparringnew.blade.php
+++ resources/views/user/usersparring/tambahsparringnew.blade.php
@@ -124,7 +124,7 @@
         </div>
         <div class="form3">
             <div class="header">
-                Aksesibilitas
+                Tingkatan
 
             </div>
             <div id="wrapper2" class="form3-wrapper">
