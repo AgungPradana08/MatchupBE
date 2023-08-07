c4d922e580367d2503965a25019a0d045612fa0d
Agung_Pradana_08 <izunna158@gmail.com>
2023-08-07 08:10:58 +0700

    [Agung] Improve

--- app/Http/Controllers/API/LokasiController.php
+++ app/Http/Controllers/API/LokasiController.php
@@ -17,12 +17,18 @@ class LokasiController extends Controller
         ]);
     }
 
-    public function index()
+    public function getapidatalocation()
     {
         // $response = Http::timeout(60000)->get('http://127.0.0.1:8000/api/getdatalocation');
         $response = Http::get('http://127.0.0.1:8000/api/getdatalocation'); // Ganti URL dengan URL API Anda
         $data = $response->json(); // Konversi respons API menjadi array
         $peta = $data['data']; // Simpan data lokasi dalam variabel $peta
         return view('user.usersparring.tambahsparringnew', compact('peta'));
+    }
+
+    public function index(){
+        $map = DB::table('map')->get();
+        session()->flash('page', 'pengguna');
+        return view('home.pengguna.home', compact(['map']));
     }
 }
